a71eeb60723cc1e421a4fe01e44188911519d30a
pulemet <mkspulemet@gmail.com>
2022-01-03 21:40:18 +0300

    Merge remote-tracking branch 'origin/master'

--- Errors.py
+++ Errors.py
@@ -14,7 +14,7 @@ except TypeError:
 except ValueError:
     print('Нельзя!')'''
 
-def devide(number):
+'''def devide(number):
     return 10 / number
 
 def sum_of_devided(left, right):
@@ -48,7 +48,7 @@ else:
     print('Программа выполнилась без ошибок')
 finally:
     answer_file.close()
-    print(answer_file.closed)
+    print(answer_file.closed)'''
 
 '''BRUCE_WILLIS = 42
 
@@ -61,4 +61,79 @@ leeloo = int(input_data[4])
 result = BRUCE_WILLIS * leeloo
 
 print(f'- Leeloo Dallas! Multi-pass № {result}!')'''
+
+
+'''sym_sum = 0
+line_count = 0
+try:
+    people_file = open('people.txt', 'r')
+    for i_line in people_file:
+        line_count += 1
+        lenght = len(i_line)
+        if i_line.endswith('\n'):
+            lenght -= 1
+        if lenght < 3:
+            raise BaseException('Длина {} строки меньше 3 символов'.format(line_count))
+        sym_sum += lenght
+    people_file.close()
+except FileNotFoundError:
+    print('Файл не найден.')
+finally:
+    print('Найденная сумма символов: ', sym_sum)'''
+
+'''names_list = []
+
+while True:
+    try:
+        name = input('Введите имя: ')
+        if name == 'error':
+            raise BaseException('Ты сломал программу')
+        if not name.isalpha():
+            raise TypeError
+        names_list.append(name)
+        if len(names_list) == 5:
+            print('Место закончилось')
+            break
+    except TypeError:
+        print('Ты чего ввел?')
+    except BaseException:
+        names_list = []
+        print('Введено стоп-слово')
+        raise TypeError('Не вводите стоп-слово')'''
+
+'''good_log = open('registrations_good.log', 'w')
+bad_log = open('registrations_bad.log', 'w')
+file = open('registrations.txt', 'r', encoding='utf-8')
+for i_line in file.readlines():
+    try:
+        new_line = i_line.split()
+        index = name = syntax = value = False
+        if len(new_line) == 3:
+            index = True
+        if 10 < int(new_line[2]) < 99:
+            syntax = True
+        if new_line[0].isalpha() is True:
+            name = True
+        if '@' and '.' in new_line[1]:
+            syntax = True
+    except IndexError:
+        print('НЕ присутствуют все три поля: IndexError')
+    except ValueError:
+        print('поле возраст НЕ является числом от 10 до 99: ValueError')
+    except NameError:
+        print('поле имени содержит НЕ только буквы: NameError')
+    except SyntaxError:
+        print('поле емейл НЕ содержит @ и .(точку): SyntaxError')'''
+
+
+class Add():
+    def __init__(self, argument):
+        self.argument = argument
+
+    def __add__(self, other):
+        return other + self.argument
+z = Add(' water ')
+x = Add(' fire ')
+t = z + x
+print(t)
 
--- main.py
+++ main.py
@@ -25,7 +25,6 @@ for i_grade in student_info[4:]:
 for i in student:
     print(i, '-', student[i])'''
 
-
 '''num = int(input('Введите число: '))
 dictt = {i: i**2 for i in range(1, num+1)}
 print(dictt)'''
@@ -53,7 +52,6 @@ while count < 10:
     contacts[k] = v
     count += 1'''
 
-
 '''def histogram(string):
     sym_dict = dict()
     for sym in string:
@@ -70,7 +68,6 @@ hist = histogram(text)
 print('\nОригинальный словарь частот:')
 for i_sym in hist.keys():
     print(i_sym, ':', hist[i_sym])'''
-
 
 '''my_list = [(1234, 100.23), (345, 10.45), (1234, 75.00),
            (345, 222.66), (678, 300.25), (1234, 35.67)]
@@ -94,14 +91,12 @@ for k, v in collections.Counter(text).items():
 for i in inv_original:
     print(i, ':', inv_original[i])'''
 
-
 '''a = input()
 d = {}
 for i in a:
     d[i] = d.get(i, 0) + 1
 for i in sorted(d):
     print(i, ':', d[i])'''
-
 
 '''nums = int(input('Введите количество заказов: '))
 orders = dict()
@@ -137,7 +132,7 @@ print(*a)'''
 for i in range(len(let)):
     let.insert(-1, let.pop(0))
     print(let)'''
-b = 'quantity'
+'''b = 'quantity'
 a = {
     ('quantity', 'price'): 510,
     ('jhgf', 'pri'): 520
@@ -146,4 +141,182 @@ for i in a.keys():
     if b == i[0]:
         print('OK')
     print(i[0])
-print(list(a.items())[0][0][0])
+print(list(a.items())[0][0][0])'''
+
+'''import random
+
+
+class Toyota:
+    color = 'red'
+    price = 1000000
+    max_speed = 200
+    current_speed = 0
+
+
+current_speed_1 = Toyota()
+current_speed_2 = Toyota()
+current_speed_3 = Toyota()
+current_speed_1.current_speed = random.randint(0, 200)
+current_speed_2.current_speed = random.randint(0, 200)
+current_speed_3.current_speed = random.randint(0, 200)
+print(current_speed_1.current_speed)
+print(current_speed_2.current_speed)
+print(current_speed_3.current_speed)'''
+
+'''class Monitors:
+    monitor_name = 'Samsung'
+    monitor_matrix = 'VA'
+    monitor_res = 'WQHD'
+    monitor_freq = 60
+
+class Headphones:
+    headphones_name = 'Sony'
+    headphones_sensitivity = 108
+    headphones_micro = True
+
+
+monitor_freq_1 = Monitors()
+monitor_freq_2 = Monitors()
+monitor_freq_3 = Monitors()
+monitor_freq_4 = Monitors()
+
+monitor_freq_2.monitor_freq = 144
+monitor_freq_3.monitor_freq = 70
+
+headphones_micro_1 = Headphones()
+headphones_micro_2 = Headphones()
+headphones_micro_3 = Headphones()
+
+headphones_micro_1.headphones_micro = False
+
+print(monitor_freq_1.monitor_freq)
+print(monitor_freq_2.monitor_freq)
+print(monitor_freq_3.monitor_freq)
+print(monitor_freq_4.monitor_freq)
+print()
+print(headphones_micro_1.headphones_micro)
+print(headphones_micro_2.headphones_micro)
+print(headphones_micro_3.headphones_micro)'''
+
+
+'''class Car:
+    model = 'Peugeot'
+    engine = 2.0'''
+
+
+'''class User:
+    user_name = 'Admin'
+    password = 'qwerty'
+    is_banned = False
+    friends = []
+
+
+    def print_info(self):
+        print(
+            'Name: {}\nPassword: {}\nBan_dtatus: {}'.format(
+                self.user_name, self.password, self.is_banned)
+        )
+
+
+    def add_friend(self, friend):
+        if isinstance(friend, User):
+            self.friends.append(friend.user_name)
+        else:
+            self.friends.append(friend)
+
+
+
+user_1 = User()
+user_2 = User()
+user_2.user_name = 'Alina'
+user_1.add_friend('Bob')
+user_1.add_friend(user_2)
+print(user_1.friends)'''
+
+
+'''class Family:
+    surname = 'Common family'
+    money = 100000
+    have_a_house = False
+
+    def info(self):
+        print(
+            'Family name: {}\nFamily founds: {}\nhaving a house: {}\n'.format(
+                self.surname, self.money, self.have_a_house
+            )
+        )
+
+    def earn_money(self, amount):
+        self.money += amount
+        print('Earned {} money! Current value'.format(amount, self.money))
+
+
+    def buy_house(self, house_price, discount=0):
+        house_price -= house_price * discount / 100
+        if self.money>= house_price:
+            self.money -= house_price
+            self.have_a_house = True
+            print('House purchased! Current money: {}\n'.format(self.money))
+        else:
+            print('Not enough money\n')
+
+my_family = Family()
+my_family.info()
+
+print('Try to buy a house')
+my_family.buy_house(10 ** 6)
+
+if not my_family.have_a_house:
+    my_family.earn_money(800000)
+    print('Try to buy a house again')
+    my_family.buy_house(10 ** 6, 10)
+
+my_family.info()'''
+
+'''import random
+
+
+class Warrior:
+    def __init__(self, name, health=100):
+        self.name = name
+        self.health = health
+        print('Создан воин {} со здоровьем {}'.format(self.name, self.health))
+
+    def strike(self, enemyWarrior):
+        if True:
+            print('Воин {} нанес урон -20 воину {}'.format(self.name, enemyWarrior.name))
+            enemyWarrior.setHealth(enemyWarrior.getHealth() - 20)
+
+    def setHealth(self, health):
+        self.health = health
+        print('Установленно здоровье {} для воина {}'.format(self.health, self.name))
+
+    def getHealth(self):
+        try:
+            return self.health
+            print('Здоровье воина {} — {}'.format(self.name, self.health))
+        except:
+            return 'Здоровье не заданно'
+            print('Здоровье для воина {} не заданно'.format(self.name))
+
+
+one = Warrior('Георгий', 100)
+two = Warrior('Луиза', 100)
+
+while (one.health > 0) and (two.health > 0):
+    round = random.randint(1, 2)
+
+    if round == 1:
+        one.strike(two)
+    elif round == 2:
+        two.strike(one)
+
+if round == 1:
+    name = one.name
+    enemy_name = two.name
+elif round == 2:
+    name = two.name
+    enemy_name = one.name
+
+print('Поздравляем воина {}! Он одержал победу над воином {}'.format(name, enemy_name))'''
+
