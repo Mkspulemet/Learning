4dd346998bd887dae96a83bbc21ed4bc184817ab
pulemet <mkspulemet@gmail.com>
2022-01-08 20:12:51 +0300

    ;'lk

--- Tg_bot/Commands/help.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Union, List
-
-
-def help(config: Dict[str, Union[Dict[str, Dict[str, Union[str, List]]], str]]) -> str:
-    commands: str = 'Список доступных команд:\n'
-    for index, command in enumerate(config['intents']['commands']):
-        commands += f'{index + 1}. {command}\n'
-    return commands
