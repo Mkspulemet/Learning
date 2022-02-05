584e9b57bb6faf27d946cfa7f9343188e5b3be39
pulemet <mkspulemet@gmail.com>
2022-02-06 00:17:05 +0300

    Merge remote-tracking branch 'origin/master'

--- пробы/main.py
+++ пробы/main.py
@@ -1,4 +1,9 @@
+from datetime import date, datetime
+from unittest.mock import call
+
 import telebot, wikipedia, re
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+import calendar
 from testing import *
 
 db.connect()
@@ -10,6 +15,11 @@ bot = telebot.TeleBot('5056695729:AAEFCytVriAqwrskJaFYKgWqs0H1nXRs2vE')
 # Устанавливаем русский язык в Wikipedia
 wikipedia.set_lang("ru")
 
+
+querystring = {"adults1": "1", "pageNumber": "1", "pageSize": "10",
+               "checkIn": str(),
+               "checkOut": str(),
+               "currency": "USD", "locale": "ru_RU"}
 
 
 # Чистим текст статьи в Wikipedia и ограничиваем его тысячей символов
@@ -69,11 +79,68 @@ def history_out(message, res=False):
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     bot.send_message(message.chat.id, getwiki(message.text))
+    bot.send_message(message.chat.id, 'Введите дату заселения')
+    bot.register_next_step_handler(message, start)
+    # querystring["checkIn"] = None
     history(message)
-    print(message.from_user.id)
+
+
+# @bot.message_handler()
+# def start(m):
+#     calendar, step = DetailedTelegramCalendar(min_date=date.today()).build()
+#     bot.send_message(m.chat.id,
+#                      f"Select {LSTEP[step]}",
+#                      reply_markup=calendar)
+#
+#
+# @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+# def cal(c):
+#     result, key, step = DetailedTelegramCalendar(min_date=date.today()).process(c.data)
+#     if not result and key:
+#         print('qwerty')
+#         bot.edit_message_text(f"Select {LSTEP[step]}",
+#                               c.message.chat.id,
+#                               c.message.message_id,
+#                               reply_markup=key)
+#     elif result:
+#         bot.edit_message_text(f"{result}",
+#                               c.message.chat.id,
+#                               c.message.message_id)
+#     return result
+
+
+
+
 
 
 # Запускаем бота
+
+current_shown_dates = {}
+@bot.message_handler(commands=['calendar'])
+def get_calendar(message):
+    now = datetime.datetime.now() #Current date
+    chat_id = message.chat.id
+    date = (now.year, now.month)
+    current_shown_dates[chat_id] = date #Saving the current date in a dict
+    markup = create_calendar(now.year, now.month)
+    bot.send_message(message.chat.id, "Пожалуйста, выберите дату", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data[0:13] == 'calendar-day-')
+def get_day(call):
+    chat_id = call.message.chat.id
+    bot.register_next_step_handler(call, date_for)
+def date_for(chat_id):
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        day = call.data[13:]
+        date = datetime.date(int(saved_date[0]), int(saved_date[1]), int(day))
+        bot.send_message(chat_id, str(date))
+        bot.answer_callback_query(call.id, text="Выбрана дата")
+        return date
+    else:
+        bot.send_message(chat_id, 'Ошибка')
+        pass
+
 bot.polling(none_stop=True, interval=0)
 
 
